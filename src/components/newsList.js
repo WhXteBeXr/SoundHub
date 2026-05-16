0ed0014ad050bcd8a59fce32e729506991e7f3a1
WhXteBeXr <WhiteBear_LT@outlook.com>
2026-05-17 01:53:04 +0300

    Added adaptive for the site

--- src/components/newsList.js
+++ src/components/newsList.js
@@ -10,15 +10,15 @@ export function NewsList(images = []) {
     <section class="news-list block-contrast-cascade-shadows">
       <div class="news-list__item">
         <img src="${images[0]}" class="news-list__image" alt="">
-        <p class="news-list__caption focusable-element">News text</p>
+        <p class="news-list__caption focusable-element">Ableton не купили у нас рекламу, а очень жаль :(</p>
       </div>
       <div class="news-list__item">
         <img src="${images[1]}" class="news-list__image" alt="">
-        <p class="news-list__caption">News text</p>
+        <p class="news-list__caption">Dolby Atmos делает звук очень классным!</p>
       </div>
       <div class="news-list__item">
         <img src="${images[2]}" class="news-list__image" alt="">
-        <p class="news-list__caption">News text</p>
+        <p class="news-list__caption">Spotify наш спонсор... Хотелось бы нам сказать, но нет</p>
       </div>
     </section>
   `;
